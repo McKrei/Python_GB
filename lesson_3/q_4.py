266d151534c3961bf49fcc4d445e7d59a12be7d8
Sergunin_Evgeniy <mckrei@ya.ru>
2021-11-30 15:04:12 +0300

    Уроки 4 и 5

---
+++ lesson_3/q_4.py
@@ -0,0 +1,39 @@
+'''
+4. Программа принимает действительное положительное число x и целое отрицательное число
+y. Выполните возведение числа x в степень y. Задание реализуйте в виде функции
+my_func(x, y). При решении задания нужно обойтись без встроенной функции возведения
+числа в степень.
+Подсказка: попробуйте решить задачу двумя способами. Первый — возведение в степень с
+помощью оператора **. Второй — более сложная реализация без оператора **,
+предусматривающая использование цикла.
+'''
+
+while True:
+	try:
+		plus_number = int(input("Введите положительное число: "))
+	except:
+		continue
+	while True:
+		try:
+			minus_number = int(input("Введите отрицательное число: "))
+			if minus_number < 0:
+				break
+			continue
+		except:
+			continue
+	break
+
+def expo(num_1, num_2):
+	global number
+	for i in range(1, abs(num_2)):		
+		num_1 *= num_1
+	number = num_1
+	return 1 / num_1
+
+print(f'Получилось: {expo(plus_number, minus_number)}, В виде дроби: 1/{number}') 
+
+# Две строчки по первому варианту
+
+my_func = lambda x, y: x ** y
+print(f'Решение питона: {my_func(plus_number, minus_number)}') 
+
